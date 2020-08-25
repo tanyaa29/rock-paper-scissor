895b430f50616a50ee0345a1cfe7c3f94079dae0
Tanya <tanyachapahadi@gmail.com>
2020-08-25 16:25:41 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onload=function() {
+const userscore=0;
+const computerscore=0;
+const userscore_span=document.getElementById("user-score");
+const computerscore_span=document.getElementById("computer-score");
+const scoreboard_div= document.querySelector(".score-board");
+const result_div= document.querySelector(".result");
+const rock_div= document.getElementById("r");
+const paper_div= document.getElementById("p");
+const scissor_div= document.getElementById("s");
+
+    
+
+
+console.log("hi00");
+}
